cdf295aae75944c12a3d66c6a161c95c4f88b3fd
Subhash Rawat <subhash@techmatys.com>
2022-03-04 12:04:36 +0530

    Minor changes

--- PMS.Infrastructure/Repositories/EmployeePaymentRepository.cs
+++ PMS.Infrastructure/Repositories/EmployeePaymentRepository.cs
@@ -86,7 +86,6 @@ namespace PMS.Infrastructure.Repositories
                     {
                         fields.EmployeeId,
                         fields.Amount,
-
                         fields.PaymentMonthYear,
                         fields.PaymentDate,
                         fields.Notes                        
