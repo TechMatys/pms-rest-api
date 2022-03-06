152aee5f5335cdb49fd4098a9b1d2f0b4c8e69bd
PRAKASHRAWAT33 <86583004+PRAKASHRAWAT33@users.noreply.github.com>
2022-03-06 10:27:37 +0530

    Updated Project-Payment working

--- PMS.Core/Model/ProjectPaymentModal.cs
+++ PMS.Core/Model/ProjectPaymentModal.cs
@@ -4,20 +4,23 @@ namespace PMS.Core.Model
 {
     public class ProjectPaymentListModel
     {
+        public int ProjectPaymentId { get; set; }
         public int ProjectName { get; set; }
         public string? ReceivedAmount { get; set; }
         public string? PaymentMonthYear { get; set; }
         public string? PaymentDate { get; set; }
+        public string? CreatedBy { get; set; }
     }
 
     public class ProjectPayment
     {
+        public int ProjectPaymentId { get; set; }
         public int ProjectId { get; set; }
-        public string? ReceivedAmount { get; set; }
+        public string? RecievedAmount { get; set; }
         public string? BalancedAmount { get; set; }
         public string? PaymentMonthYear { get; set; }
         public string? PaymentDate { get; set; }
-        
-        
+        public int? ManagedBy { get; set; }
+
     }
 }
